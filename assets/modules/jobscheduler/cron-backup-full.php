13424483a6f0d6d3b089565e9bcc597b924926bb
Nicola Lambathakis <modxit@modx.it>
2017-02-07 14:21:53 +0100

    example for full backup

---
+++ assets/modules/jobscheduler/cron-backup-full.php
@@ -0,0 +1,15 @@
+<?php
+define('MODX_API_MODE', true);
+include_once(dirname(__FILE__)."../../../../index.php");
+$modx->db->connect();
+if (empty ($modx->config)) {
+    $modx->getSettings();
+}
+$modx->invokeEvent("OnWebPageInit");
+$message = "Full site Backup Successfull";
+
+$out = $modx->runSnippet('CronEvoBackup', array('mode' => 'full', 'zipdb' => '1', 'number_of_backups' => '10'));
+$return = array('message' => $out);
+//echo json_encode($return);
+echo $message;
+?>
